47890a0540a3ef50b369d26202cdd120e231c2d3
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-07-16 19:03:59 -0300

    feat(csv): escreve nova linha no csv

--- service/serviceEstoque.ts
+++ service/serviceEstoque.ts
@@ -1,11 +1,12 @@
 const user_input = require('prompt-sync')({sigint: true})
+import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
 
 
 interface NewRow {
     nome: string,
     peso: number,
     valor: number,
-    quantidade: number
+    quant: number
 }
 
 
@@ -17,4 +18,18 @@ function readNewRow(): NewRow {
         quantidade: user_input('Digite a quantidade: ')
     }
     return new_row
+}
+
+async function writeNewRow(file_path: string, new_row: NewRow[]): Promise<void> {
+    const csv_writer = createCsvWriter({
+        path: file_path,
+        header: [
+            {id: 'nome', title: 'Nome'},
+            {id: 'peso',title: 'Peso'},
+            {id: 'valor',title: 'Valor'},
+            {id: 'quant',title: 'Quantidade'},
+        ],
+    })
+
+    return csv_writer.writeRecords(new_row)
 }
