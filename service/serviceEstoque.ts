84cc06aa8dc78d029899aa681483db9c6de634d6
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-07-17 17:14:02 -0300

    feat: verifica novo item do prompt

--- service/serviceEstoque.ts
+++ service/serviceEstoque.ts
@@ -2,6 +2,21 @@ import { readCSV } from '../model/readCSV'
 import { writeCSV } from '../model/writeCSV'
 import { NewRow } from '../model/interfaceData'
 
-const file_path = '../db/estoque.csv'
+
+const dbPath = '../db/estoque.csv'
 
 
+async function verifyRow(userRow: string[]): Promise<boolean> {
+    if (typeof userRow[0] !== 'string' || isNaN(Number(userRow[1])) || isNaN(Number(userRow[2])) || 
+    isNaN(Number(userRow[3]))) {
+        return false
+    }
+    const rows = await readCSV(dbPath)
+
+    for (let i=0; i<4; i++){
+        if (rows[i].nome === userRow[0]){
+            return false
+        }
+    }
+    return true
+}
