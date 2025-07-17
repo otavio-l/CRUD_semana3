b8114947135cab0446da5210e1f9274683cf355b
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-07-17 10:49:00 -0300

    feat(csv): adiciona removeRow() e parametro append no writeCSV()

--- model/writeCSV.ts
+++ model/writeCSV.ts
@@ -1,9 +1,10 @@
 import { NewRow } from './interfaceData';
+import { readCSV } from './readCSV';
 
 import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
 
 
-export async function writeCSV(file_path: string, new_row: NewRow[]): Promise<void> {
+export async function writeCSV(file_path: string, new_row: NewRow[], append = true): Promise<void> {
     const csv_writer = createCsvWriter({
         path: file_path,
         header: [
@@ -12,7 +13,14 @@ export async function writeCSV(file_path: string, new_row: NewRow[]): Promise<vo
             {id: 'valor',title: 'Valor'},
             {id: 'quant',title: 'Quantidade'},
         ],
+        append: append
     })
 
     return csv_writer.writeRecords(new_row)
+}
+
+export async function removeRow(file_path: string, name_exlclude: string): Promise<void> {
+    const rows = await readCSV(file_path)
+    const updated_rows = rows.filter(current_row => !(current_row.nome == name_exlclude))
+    await writeCSV(file_path, updated_rows, false);
 }
