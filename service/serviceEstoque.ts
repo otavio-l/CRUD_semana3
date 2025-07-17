44327acd8a02071d5791dcff040f6f2e50b308f1
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-07-17 17:16:16 -0300

    feat: verifica novo item do prompt

--- service/serviceEstoque.ts
+++ service/serviceEstoque.ts
@@ -6,7 +6,7 @@ import { NewRow } from '../model/interfaceData'
 const dbPath = '../db/estoque.csv'
 
 
-async function verifyRow(userRow: string[]): Promise<boolean> {
+export async function verifyRow(userRow: string[]): Promise<boolean> {
     if (typeof userRow[0] !== 'string' || isNaN(Number(userRow[1])) || isNaN(Number(userRow[2])) || 
     isNaN(Number(userRow[3]))) {
         return false
