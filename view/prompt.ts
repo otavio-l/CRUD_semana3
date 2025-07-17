1f604d3275c9578f5aac0d7225bf16d22d5952d5
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-07-17 15:52:49 -0300

    refactor: troca de NewRow para array na captura de user input

--- view/prompt.ts
+++ view/prompt.ts
@@ -3,12 +3,12 @@ import { NewRow } from '../model/interfaceData';
 const user_input = require('prompt-sync')({sigint: true})
 
 
-export function readNewRow(): NewRow {
-    const new_row = {
-        nome: user_input('Digite o nome: '),
-        peso: user_input('Digite o peso: '),
-        valor: user_input('Digite o valor: '),
-        quant: user_input('Digite a quantidade: ')
-    }
-    return new_row
+export function readNewRow(): string[] {
+    const userRow: string[] = []
+    userRow.push(user_input('Digite o nome: '))
+    userRow.push(user_input('Digite o nome: '))
+    userRow.push(user_input('Digite o peso: '))
+    userRow.push(user_input('Digite o valor: '))
+    userRow.push(user_input('Digite a quantidade: '))
+    return userRow
 }
