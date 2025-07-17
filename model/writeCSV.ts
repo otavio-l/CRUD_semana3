e5b47e258571604ac5b6ecce505cbceaee3c3a4c
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-07-17 13:09:28 -0300

    refactor: renaming to camelCase

--- model/writeCSV.ts
+++ model/writeCSV.ts
@@ -1,12 +1,12 @@
 import { NewRow } from './interfaceData';
 import { readCSV } from './readCSV';
 
-import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
+import { createObjectCsvWriter } from 'csv-writer';
 
 
-export async function writeCSV(file_path: string, new_row: NewRow[], append = true): Promise<void> {
-    const csv_writer = createCsvWriter({
-        path: file_path,
+export async function writeCSV(dbPath: string, row: NewRow[], append = true): Promise<void> {
+    const csvWriter = createObjectCsvWriter({
+        path: dbPath,
         header: [
             {id: 'nome', title: 'Nome'},
             {id: 'peso',title: 'Peso'},
@@ -16,11 +16,11 @@ export async function writeCSV(file_path: string, new_row: NewRow[], append = tr
         append: append,
     })
 
-    return csv_writer.writeRecords(new_row)
+    return csvWriter.writeRecords(row)
 }
 
-export async function removeRow(file_path: string, name_exlclude: string): Promise<void> {
-    const rows = await readCSV(file_path)
-    const updated_rows = rows.filter(current_row => current_row.nome !== name_exlclude)
-    await writeCSV(file_path, updated_rows, false);
+export async function removeRow(dbPath: string, nameExclude: string): Promise<void> {
+    const rows = await readCSV(dbPath)
+    const updatedRows = rows.filter(currentRow => currentRow.nome !== nameExclude)
+    await writeCSV(dbPath, updatedRows, false);
 }
