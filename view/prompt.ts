4e86ebba83a6ca1c86a92cfd3b65bf63fbd5f9c8
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-07-17 23:54:30 -0300

    feat(view): adiciona certeza na remoção de uma linha

--- view/prompt.ts
+++ view/prompt.ts
@@ -1,14 +1,20 @@
 import { NewRow } from '../model/interfaceData';
 
-const user_input = require('prompt-sync')({sigint: true})
+const userInput = require('prompt-sync')({sigint: true})
 
 
 export function readNewRow(): string[] {
     const userRow: string[] = []
-    userRow.push(user_input('Digite o nome: '))
-    userRow.push(user_input('Digite o nome: '))
-    userRow.push(user_input('Digite o peso: '))
-    userRow.push(user_input('Digite o valor: '))
-    userRow.push(user_input('Digite a quantidade: '))
+    userRow.push(userInput('Digite o nome: '))
+    userRow.push(userInput('Digite o nome: '))
+    userRow.push(userInput('Digite o peso: '))
+    userRow.push(userInput('Digite o valor: '))
+    userRow.push(userInput('Digite a quantidade: '))
     return userRow
+}
+
+export function removeCertainty(row: NewRow): void {
+    console.log("Você está prestes a excluir essa linha:")
+    console.log(Object.values(row).join(' , '))
+    const remove = userInput("Tem certeza?[S/N]: ")
 }
