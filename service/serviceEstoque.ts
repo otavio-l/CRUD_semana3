c6ccb679cffa1483ff3ce43e1bdddef9fd578d9c
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-07-18 13:15:10 -0300

    fix(service): corrige for loop quando csv não tem 4 linhas

--- service/serviceEstoque.ts
+++ service/serviceEstoque.ts
@@ -4,14 +4,15 @@ import { readCSV } from '../model/readCSV'
 export async function verifyRow(userRow: string[]): Promise<string[]> {
     const [nome, peso, valor, quant] = userRow
     
-    if (typeof userRow[0] !== 'string' || isNaN(Number(peso)) || isNaN(Number(valor)) || 
+    if (typeof nome !== 'string' || isNaN(Number(peso)) || isNaN(Number(valor)) || 
     isNaN(Number(quant))) {
         throw new Error('Dados inválidos para o produto')
     }
     
     const rows = await readCSV()
-    for (let i=0; i<4; i++){
-        if (rows[i].nome === userRow[0]){
+    
+    for (let i=0; i<rows.length; i++){
+        if (rows[i].nome === nome){
             throw new Error('Produto já existe')
         }
     }
